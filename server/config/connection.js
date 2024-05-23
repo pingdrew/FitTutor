ffb3de504316ecfff25d9e9fc179f4ab0c2a8dc6
pingdrew <pingdrew0@gmail.com>
2024-05-23 12:00:05 -0500

    attempting to fix heroku issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-const connectionURI = process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@pingdrew2048.0hwklu7.mongodb.net/test?retryWrites=true&w=majority`
+const connectionURI = process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@pingdrew2048.0hwklu7.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(connectionURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  ssl: true,
-  sslValidate: true,
-  sslCA: Buffer.from(process.env.CA_CERTIFICATE_BASE64, 'base64')
+  tls: true, // Use tls for secure connections instead of ssl
 });
 
 module.exports = mongoose.connection;
